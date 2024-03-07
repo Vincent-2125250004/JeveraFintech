24a7491181a2301642f3e9a58c132ee33aba8b81
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2024-03-07 12:18:40 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            $this->app['request']->server->set('HTTPS', 'on'); // this line
+
+            URL::forceScheme('https');
+        }
+
+        config(['app.locale' => 'id']);
+        Carbon::setLocale('id');
     }
 }
