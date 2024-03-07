91d826e36d97beb75c8e3045a6034ce6292c20a2
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2024-03-07 11:40:04 +0700

    Change Vite CSS JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,10 @@
         }
     </script>
 
+    <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
+    
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 
 <body class="font-sans antialiased">
@@ -48,5 +50,5 @@
     </div>
 
 </body>
-
+    <script src="{{ asset('resources/js/app.js') }}"></script>
 </html>
